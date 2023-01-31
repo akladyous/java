2adb606ad38c7470941044e36d5fd736ee4c0da3
Boula Akladyous <akladyous@gmail.com>
2023-01-31 16:52:50 -0500

    java

---
+++ practice/src/com/practice/interfaces/TempConverterTest.java
@@ -0,0 +1,16 @@
+package com.practice.interfaces;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TempConverterTest {
+
+    TemperatureConverter converter = new TempConverter();
+    @Test
+    void celciusToFahrenheit(){
+        double result = this.converter.celciusToFahrenheit(100);
+        Assertions.assertEquals(212, result, 0.1);
+    }
+}
