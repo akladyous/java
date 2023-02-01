114c301d0171ef04db8f1ae0b589e9ce2f656a94
Boula Akladyous <akladyous@gmail.com>
2023-02-01 18:26:18 -0500

    java

--- practice/src/com/practice/strings/StringConcatenation.java
+++ practice/src/com/practice/strings/StringConcatenation.java
@@ -3,4 +3,14 @@ package com.practice.strings;
 public class StringConcatenation {
     String newLine = System.getProperty("line.separator");
 
+    public String concat1(){
+        return "Get busy living"
+                .concat(newLine)
+                .concat("or")
+                .concat(newLine)
+                .concat("get busy dying.")
+                .concat(newLine)
+                .concat("--Stephen King");
+    }
+
 }
