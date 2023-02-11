3f69118577f0e95885f4e02838d14327e0171d33
Boula Akladyous <akladyous@gmail.com>
2023-02-10 21:35:41 -0500

    java

--- practice/src/com/practice/map/HashMapDemo.java
+++ practice/src/com/practice/map/HashMapDemo.java
@@ -1,4 +1,20 @@
 package com.practice.map;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapDemo {
+
+    public static void main(String[] args) {
+        Map<String, String> users = new HashMap<>();
+        users.put("name", "paolo");
+
+        // hashMap with multiple data types
+        Map<String, Object> mapObj = new HashMap<>();
+        mapObj.put("age", 20);
+        mapObj.put("name", "john");
+        mapObj.put("isActive", true);
+
+    }
 }
