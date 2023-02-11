b84d210d0188d9a091408b6255ce5e1f4e4169b2
Boula Akladyous <akladyous@gmail.com>
2023-02-11 11:26:27 -0500

    java

--- practice/src/com/practice/map/HashMapDemo.java
+++ practice/src/com/practice/map/HashMapDemo.java
@@ -1,20 +1,33 @@
 package com.practice.map;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class HashMapDemo {
 
     public static void main(String[] args) {
         Map<String, String> users = new HashMap<>();
         users.put("name", "paolo");
-
-        // hashMap with multiple data types
+        // hashMap with multiple Values data types
         Map<String, Object> mapObj = new HashMap<>();
         mapObj.put("age", 20);
         mapObj.put("name", "john");
         mapObj.put("isActive", true);
+        mapObj.put("test", 1234);
+        Set mapObjEntrySet = mapObj.entrySet();
+        Iterator mapObjIterator = mapObjEntrySet.iterator();
+
+        System.out.println(mapObj);
+        System.out.println(mapObj.containsKey("name"));
+        System.out.println(mapObj.containsValue("john"));
+        System.out.println(mapObj.containsValue(20));
+        System.out.println(mapObj.remove("test"));
+        System.out.println("mapObj.entrySet() : " + mapObjEntrySet);
+
 
     }
+    public void prova(){};
+    public static void PrintHashMap(HashMap obj){
+
+    }
+
 }
