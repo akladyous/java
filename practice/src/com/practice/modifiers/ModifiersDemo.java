2c395e79174d4db6b09fc1866bdcb6a71539e794
Boula Akladyous <akladyous@gmail.com>
2023-02-13 16:08:26 -0500

    java

--- practice/src/com/practice/modifiers/ModifiersDemo.java
+++ practice/src/com/practice/modifiers/ModifiersDemo.java
@@ -2,7 +2,7 @@ package com.practice.modifiers;
 
 public class ModifiersDemo {
     final static Integer varInt = 1;
-    final static String varStr = new String("hello");
+    static String varStr = new String("hello");
     public static void main(String[] args) {
         varStr = "sdfsdfsd";
     }
