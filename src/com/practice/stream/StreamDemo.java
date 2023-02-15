76a430f27a9c441cf33388705bff81382c6c2421
Boula Akladyous <akladyous@gmail.com>
2023-02-15 16:24:07 -0500

    Untracked files on java

--- src/com/practice/stream/StreamDemo.java
+++ src/com/practice/stream/StreamDemo.java
@@ -1,6 +1,15 @@
 package com.practice.stream;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class StreamDemo {
+
+  static Stream<String> streamFromValues = Stream.of("one", "two", "three");
+
+  static String[] arrayOfStrings = { "a1", "a2", "a3" };
+  static Stream<String> streamFromArrays1 = Arrays.stream(arrayOfStrings);
+  static Stream<String> streamFromArrays2 = Stream.of(arrayOfStrings);
 
   public static void main(String[] args) {
 
