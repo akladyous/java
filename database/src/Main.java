5fad09c6c0e82e793fabe6fa9f829f912a7bb564
Boula Akladyous <akladyous@gmail.com>
2023-02-16 16:55:40 -0500

    Untracked files on java

---
+++ database/src/Main.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        String query = SqlConnect.parseSqlFile("test.sql");
+        System.out.println("query \n " + query);
+        Connection conn = SqlConnect.dbConnect("boula", "paolo");
+    }
+
+    public static void prova() {
+        BufferedReader reader = null;
+        File script;
+        StringBuilder query = new StringBuilder();
+        String line;
+        try {
+            reader = new BufferedReader( new FileReader("./data/test.sql"));
+            while ( (line = reader.readLine() )!= null ) {
+                System.out.println("line : " + line);
+                query.append(line);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (reader != null) reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("-".repeat(40));
+        System.out.println("scripts : " + query);
+    }
+}
