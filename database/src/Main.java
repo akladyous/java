a38476347f6282bc2a94b62711a149f10462333a
Boula Akladyous <akladyous@gmail.com>
2023-02-17 00:09:30 -0500

    Untracked files on java

--- database/src/Main.java
+++ database/src/Main.java
@@ -1,16 +1,12 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.sql.Connection;
-import java.sql.Statement;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         String query = FileUtil.parseSQLFile("test.sql");
         System.out.println("query :  \n " + query);
         Connection conn = SqlConnect.dbConnect("boula", "paolo");
+
+
     }
 
 
