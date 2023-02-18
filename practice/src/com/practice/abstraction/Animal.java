5a11028f2272412272fa3376bf4384e36f9df014
Boula Akladyous <akladyous@gmail.com>
2023-02-18 18:57:49 -0500

    Untracked files on java

--- practice/src/com/practice/abstraction/Animal.java
+++ practice/src/com/practice/abstraction/Animal.java
@@ -1,10 +1,23 @@
 package com.practice.abstraction;
 
 public abstract class Animal {
+
     public String name;
-    public int age;
-    public void sleep(){
-        System.out.println("Zzz");
+    public Integer age;
+    public Integer speed;
+    protected Integer type;
+    private Double weight;
+
+    public Animal(String name, Integer age, Integer speed, Integer type, Double weight) {
+        this.name = name;
+        this.age = age;
+        this.speed = speed;
+        this.type = type;
+        this.weight = weight;
     }
+
+    public abstract void moved(Integer speed);
+    public abstract void makeNoise(String noise);
     public abstract void sound();
+    public void sleep(){System.out.println("Zzz");}
 }
