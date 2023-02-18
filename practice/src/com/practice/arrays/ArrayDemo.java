b384e8de26b9415fe149158b780f3554d3195d63
Boula Akladyous <akladyous@gmail.com>
2023-02-18 16:04:51 -0500

    Untracked files on java

--- practice/src/com/practice/arrays/ArrayDemo.java
+++ practice/src/com/practice/arrays/ArrayDemo.java
@@ -7,8 +7,28 @@ import java.util.*;
 public class ArrayDemo {
 
     public static void main(String[] args) {
-        int[] randomNumbers = generateRandomNumbers(1, 10);
-        System.out.println(Arrays.toString(randomNumbers));
+        int[] arrInt1 = generateRandomNumbers( 10);
+        Arrays.sort(arrInt1);
+        System.out.println(Arrays.toString(arrInt1));
+
+        int[] arrInt2 = new int[5];
+        Arrays.fill(arrInt2, 5);
+        System.out.println(Arrays.toString(arrInt2));
+
+        int[] arrInt3 = Arrays.copyOf(arrInt2, arrInt2.length + 5);
+        System.out.println(Arrays.toString(arrInt3));
+
+        Integer matchIndex;
+        String[] names = {"Alba", "Jane", "Mark", "Ralph", "David"};
+        Arrays.sort(names);
+        System.out.println("names sorted : " + Arrays.toString(names));
+        matchIndex = Arrays.binarySearch(names, "David");
+        System.out.println("matchIndex : " + matchIndex);
+
+        int[] arrInt4 = {1,2,3,4,5};
+        int[] arrInt5 = {1,2,3,4,5};
+
+
     }
 
     private static void provaOne(){
@@ -27,11 +47,11 @@ public class ArrayDemo {
         System.out.println(listInt1);
     }
 
-    private static int[] generateRandomNumbers(Integer from, Integer to) {
+    private static int[] generateRandomNumbers(Integer size) {
         Random random = new Random();
-        int[] randomNums = new int[to];
-        for (int i = 0; i < to ; i++) {
-            randomNums[i] = random.nextInt(from, to);
+        int[] randomNums = new int[size];
+        for (int i = 0; i < size ; i++) {
+            randomNums[i] = random.nextInt(100);
         }
         return randomNums;
     }
