400e14d8f43a9edec8da31dfd804634a7c895aef
Boula Akladyous <akladyous@gmail.com>
2023-02-20 14:58:18 -0500

    Untracked files on practice

--- practice/src/Main.java
+++ practice/src/Main.java
@@ -1,8 +1,24 @@
 import com.practice.generics.AddTwoNums;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+
+
   public static void main(String[] args) {
     // System.out.println("Hello world!");
+
+		List<String> seats = new ArrayList<>();
+		int searPerRows = 12;
+		int lastRow = (int) 'A' + (4 - 1);
+		for (char row = 'A'; row <= lastRow; row ++) {
+			for (int seatNum = 1; seatNum <= searPerRows; seatNum ++) {
+				seats.add(row + String.format("%02d", seatNum));
+			}
+		}
+		System.out.println(Arrays.toString(seats.toArray()));
 
   }
 }
