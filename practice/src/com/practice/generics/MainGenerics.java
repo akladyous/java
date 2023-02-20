71f8e065876b02cdb767894405b7c525f010e218
Boula Akladyous <akladyous@gmail.com>
2023-02-20 14:32:35 -0500

    Untracked files on practice

---
+++ practice/src/com/practice/generics/MainGenerics.java
@@ -0,0 +1,12 @@
+package com.practice.generics;
+
+public class MainGenerics {
+
+	public static void main(String[] args) {
+		AddTwoNums<Integer> numsInteger = new AddTwoNums<>(1, 2);
+		AddTwoNums<Double> numsDouble = new AddTwoNums<>(1.11, 2.22);
+		numsInteger.print();
+		numsDouble.print();
+
+	}
+}
