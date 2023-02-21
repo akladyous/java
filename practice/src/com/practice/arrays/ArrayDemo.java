29ec55f1e8cd2b46f197c2c1dd2b4441b664a637
Boula Akladyous <akladyous@gmail.com>
2023-02-21 12:18:11 -0500

    Untracked files on practice

--- practice/src/com/practice/arrays/ArrayDemo.java
+++ practice/src/com/practice/arrays/ArrayDemo.java
@@ -6,11 +6,21 @@ import java.util.*;
 
 public class ArrayDemo {
 
+    public static Integer xxx;
     public static void main(String[] args) {
         int[] arrInt1 = generateRandomNumbers( 10);
         /* SORT Method */
         Arrays.sort(arrInt1);
         System.out.println(Arrays.toString(arrInt1));
+
+        int matchIndex;
+        String[] arrStr1 = {"Alba", "Jane", "Mark", "Ralph", "David"};
+        Arrays.sort(arrStr1);
+        System.out.println("arrStr1 sorted : " + Arrays.toString(arrStr1));
+
+        /* BinarySearch Method */
+        matchIndex = Arrays.binarySearch(arrStr1, "David");
+        System.out.println("Arrays.binarySearch : " + matchIndex);
 
         int[] arrInt2 = new int[5];
         /* FILL Method */
@@ -20,15 +30,6 @@ public class ArrayDemo {
         int[] arrInt3 = Arrays.copyOf(arrInt2, arrInt2.length + 5);
         System.out.println(Arrays.toString(arrInt3));
 
-        Integer matchIndex;
-        String[] arrStr1 = {"Alba", "Jane", "Mark", "Ralph", "David"};
-        /* SORT Method */
-        Arrays.sort(arrStr1);
-        System.out.println("arrStr1 sorted : " + Arrays.toString(arrStr1));
-        /* BinarySearch Method */
-        matchIndex = Arrays.binarySearch(arrStr1, "David");
-        System.out.println("matchIndex : " + matchIndex);
-
         int[] arrInt4 = {1,2,3,4,5};
         int[] arrInt5 = {1,2,3,4,5};
         if (Arrays.equals(arrInt4, arrInt5)) {
@@ -36,11 +37,13 @@ public class ArrayDemo {
         } else {
             System.out.println("Arrays are not equals");
         }
-
-        ArrayList<String> ar1 = new ArrayList<>(List.of("six", "seven", "eight", "nine", "ten"));
-        ar1.addAll(ar1);
-        System.out.println("Array List added " + ar1);
-        var newArray = ar1.toArray(new String[ar1.size()]);
+        ArrayList<String>  arrNums1 = new ArrayList<>(Arrays.asList("one", "two","three", "four", "five"));
+        ArrayList<String> arrNums2 = new ArrayList<>(List.of("six", "seven", "eight", "nine", "ten"));
+        ArrayList<String> arrOfNumbers = new ArrayList<>();
+        arrOfNumbers.addAll(arrNums1);
+        arrOfNumbers.addAll(arrNums2);
+        System.out.println("arrOfNumbers.addAll : " + arrOfNumbers);
+        var newArray = arrNums1.toArray(new String[arrNums1.size()]);
         System.out.println("newArray : " + Arrays.toString(newArray));
 
 				System.out.println("-".repeat(40));
@@ -48,20 +51,20 @@ public class ArrayDemo {
         String[] arrStr3 = {"four", "five", "six"};
 				ArrayList<String> arrStr4 = new ArrayList<>();
 
-				arrStr4.addAll(Arrays.asList(arrStr2));
-				arrStr4.addAll(new ArrayList<>(List.of(arrStr3)));
-				System.out.println("arrStr4 : " + arrStr4);
-				Collections.shuffle(arrStr4);
-				Boolean deepEquals1 = Arrays.deepEquals(
-								new List[] {Arrays.asList(arrStr2)},
-								new List[] {List.of(arrStr2)}
-				);
-				Boolean deepEquals2 = Arrays.deepEquals(
-								new List[] {Arrays.asList(arrStr2)},
-								new ArrayList[] {arrStr4}
-				);
-				System.out.println("deepEquals : " + deepEquals1.toString());
-				System.out.println("deepEquals : " + deepEquals2.toString());
+        arrStr4.addAll(Arrays.asList(arrStr2));
+        arrStr4.addAll(new ArrayList<>(List.of(arrStr3)));
+        System.out.println("arrStr4 : " + arrStr4);
+        Collections.shuffle(arrStr4);
+        boolean deepEquals1 = Arrays.deepEquals(
+                        new List[] {Arrays.asList(arrStr2)},
+                        new List[] {List.of(arrStr2)}
+        );
+        Boolean deepEquals2 = Arrays.deepEquals(
+                        new List[] {Arrays.asList(arrStr2)},
+                        new ArrayList[] {arrStr4}
+        );
+        System.out.println("deepEquals : " + Boolean.toString(deepEquals1));
+        System.out.println("deepEquals : " + deepEquals2.toString());
     }
 
     private static void provaOne(){
