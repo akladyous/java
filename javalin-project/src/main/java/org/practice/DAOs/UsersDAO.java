0294e9bbf83853df522cf03a4f97e7224a0a70af
Boula Akladyous <akladyous@gmail.com>
2023-02-23 18:33:33 -0500

    Untracked files on java

--- javalin-project/src/main/java/org/practice/DAOs/UsersDAO.java
+++ javalin-project/src/main/java/org/practice/DAOs/UsersDAO.java
@@ -1,7 +1,15 @@
 package org.practice.DAOs;
 
+import org.practice.models.User;
+
+import java.util.List;
+
 public interface UsersDAO {
 
-
+    User createUser() throws DAOsException;
+    User getUser() throws DAOsException;
+    User updateUser() throws DAOsException;
+    Boolean deleteUser() throws DAOsException;
+    List<User> findAll() throws DAOsException;
 
 }
