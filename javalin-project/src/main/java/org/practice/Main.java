ab65b89bedec1b50b49d52f18821cbc4a2e47d83
Boula Akladyous <akladyous@gmail.com>
2023-02-23 17:51:37 -0500

    Untracked files on java

--- javalin-project/src/main/java/org/practice/Main.java
+++ javalin-project/src/main/java/org/practice/Main.java
@@ -1,20 +1,41 @@
 package org.practice;
 
-import io.javalin.Javalin;
-import io.javalin.apibuilder.EndpointGroup;
-
 public class Main {
     public static void main(String[] args) {
 
 
-//        var app = Javalin
-//                          .create()
-//                          .get("/", ctx -> ctx.result("ciao").status(200))
-//                          .start(4000);
+        System.out.println(reverseWord("ehT  kciuq nworb xof spmuj revo eht yzal .god"));;
+        System.out.println(reverseWord("sihT si eht tsrif .ecnetnes  sihT si eht .dnoces"));;
+        System.out.println(reverseWord("elbuod  decaps  sdrow"));;
+        System.out.println(reverseWord("elppa"));;
+        /*
+        var app = Javalin
+                          .create()
+                          .get("/", ctx -> ctx.result("ciao").status(200))
+                          .start(4000);
+         */
     }
 
 
-    public static String reverseWords(String original) {
+    public static String reverseWord(String original) {
+        String result = "";
+        String stack="";
+        int leftIndex = 0;
+        int rightIndex;
+
+        for (rightIndex = 0; rightIndex < original.length()  ; rightIndex++) {
+            if (original.charAt(rightIndex) == ' ' ) {
+                for (int idx = rightIndex - 1; idx >= leftIndex; idx --) {
+                    result += original.charAt(idx);
+                }
+                leftIndex = rightIndex + 1;
+                result += ' ';
+            }
+        }
+        return result;
+    }
+
+    public static String reverseWord1(String original) {
         String words[] = original.trim().split("\\s+");
         for (int word = 0; word < words.length; word++) {
             char[] reversedWord = new char[words[word].length()];
