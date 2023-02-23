dc989467bf3f6bd046ef68035015786c46aab5de
Boula Akladyous <akladyous@gmail.com>
2023-02-23 17:53:15 -0500

    Untracked files on java

--- javalin-project/src/main/java/org/practice/Main.java
+++ javalin-project/src/main/java/org/practice/Main.java
@@ -1,16 +1,14 @@
 package org.practice;
 
+import io.javalin.Javalin;
+
 public class Main {
     public static void main(String[] args) {
 
-
-
-        /*
         var app = Javalin
                           .create()
                           .get("/", ctx -> ctx.result("ciao").status(200))
                           .start(4000);
-         */
     }
 
 
