d0fe9d09e9783468df38b78df1d419fccab6a00d
Boula Akladyous <akladyous@gmail.com>
2023-02-23 18:20:42 -0500

    Untracked files on java

--- javalin-project/src/main/java/org/practice/DAOs/UsersDAO.java
+++ javalin-project/src/main/java/org/practice/DAOs/UsersDAO.java
@@ -3,4 +3,5 @@ package org.practice.DAOs;
 public interface UsersDAO {
 
 
+
 }
---
+++ javalin-project/src/main/java/org/practice/models/User.java
@@ -0,0 +1,125 @@
+package org.practice.models;
+
+import java.util.Objects;
+
+public class User {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Boolean active = true;
+    private Boolean verified;
+
+    public User() {};
+
+    public User(Integer id, String firstName, String lastName, String email, String password, Boolean active, Boolean verified) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.active = active;
+        this.verified = verified;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public Boolean getVerified() {
+        return verified;
+    }
+    // setters
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public void setVerified(Boolean verified) {
+        this.verified = verified;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                       "id=" + id +
+                       ", firstName='" + firstName + '\'' +
+                       ", lastName='" + lastName + '\'' +
+                       ", email='" + email + '\'' +
+                       ", password='" + password + '\'' +
+                       ", active=" + active +
+                       ", verified=" + verified +
+                       '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!Objects.equals(id, user.id)) return false;
+        if (!Objects.equals(firstName, user.firstName))
+            return false;
+        if (!Objects.equals(lastName, user.lastName))
+            return false;
+        if (!Objects.equals(email, user.email)) return false;
+        if (!Objects.equals(password, user.password))
+            return false;
+        if (!Objects.equals(active, user.active)) return false;
+        return Objects.equals(verified, user.verified);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (active != null ? active.hashCode() : 0);
+        result = 31 * result + (verified != null ? verified.hashCode() : 0);
+        return result;
+    }
+}
