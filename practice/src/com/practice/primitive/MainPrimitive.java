725db1faab41acef84294b443c88747e2dd6cf9b
Boula Akladyous <akladyous@gmail.com>
2023-03-03 16:27:42 -0500

    Untracked files on javalin-project

--- practice/src/com/practice/primitive/MainPrimitive.java
+++ practice/src/com/practice/primitive/MainPrimitive.java
@@ -1,10 +1,13 @@
 package com.practice.primitive;
 
+import java.util.HashMap;
+
 public class MainPrimitive {
 
     public static void main(String[] args) {
 
         String s1 = "ciao";
         System.out.println(String.valueOf(s1.charAt(0)));
+        HashMap<Integer> indices = new HashMap<>();
     }
 }
