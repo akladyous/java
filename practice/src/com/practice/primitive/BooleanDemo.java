55b85c00da3cf23f6266cf3bb0cb212783a27f1b
Boula Akladyous <akladyous@gmail.com>
2023-03-06 11:09:22 -0500

    Untracked files on java

--- practice/src/com/practice/primitive/BooleanDemo.java
+++ practice/src/com/practice/primitive/BooleanDemo.java
@@ -13,8 +13,7 @@ public class BooleanDemo {
     }
     System.out.println("Boolean.valueOf() is not null : " + Boolean.valueOf(boolStr));
     System.out.println("Boolean.compare               : " + Boolean.compare(true, false));
-    System.out
-        .println("compareTo                     : " + Boolean.valueOf(boolTrue).compareTo(Boolean.valueOf(boolFalse)));
+    System.out.println("compareTo                     : " + Boolean.valueOf(boolTrue).compareTo(Boolean.valueOf(boolFalse)));
     System.out.println("Boolean.logicalAnd            : " + Boolean.logicalAnd(true, true));
     System.out.println("Boolean.getBoolean            : " + Boolean.getBoolean(boolStr));
     System.out.println("Boolean.parse(string value)   : " + Boolean.parseBoolean(boolStr));
