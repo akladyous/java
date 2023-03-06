bd2f9b2480e12282391dac2796c226361d7dd1e8
Boula Akladyous <akladyous@gmail.com>
2023-03-06 10:02:50 -0500

    Untracked files on java

--- practice/src/com/practice/primitive/MainPrimitive.java
+++ practice/src/com/practice/primitive/MainPrimitive.java
@@ -6,8 +6,5 @@ public class MainPrimitive {
 
     public static void main(String[] args) {
 
-        String s1 = "ciao";
-        System.out.println(String.valueOf(s1.charAt(0)));
-        HashMap<Integer> indices = new HashMap<>();
     }
 }
