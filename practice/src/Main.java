5cda7c6cfdd4a0841a47c8d0cfbe886a0c1d21a9
Boula Akladyous <akladyous@gmail.com>
2023-03-07 15:06:38 -0500

    Untracked files on java

--- practice/src/Main.java
+++ practice/src/Main.java
@@ -16,7 +16,17 @@ public class Main {
 		//System.out.println(Arrays.toString(seats.toArray()));
 
 
-
+    System.out.println("multiply by 2");
+    for (int i=1; i <=10 ; i++) {
+      int multiply = i << 1;
+      System.out.format("%d << %-4d", i, multiply);
+    }
+    System.out.println();
+    System.out.println("divide bt 2");
+    for (int i=1; i <=10 ; i++) {
+      int divide = i >> 1;
+      System.out.format("%d << %-4d", i, divide);
+    }
 
   }
 
