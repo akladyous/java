0614b1b76c2f1453d5d03eb88aaeb44b7a4a2871
Boula Akladyous <akladyous@gmail.com>
2023-03-07 10:02:25 -0500

    Untracked files on java

--- practice/src/com/practice/primitive/CharacterDemo.java
+++ practice/src/com/practice/primitive/CharacterDemo.java
@@ -5,9 +5,16 @@ package com.practice.primitive;
 public class CharacterDemo {
     public static void main(String[] args) {
         char c1 = 'a';
+        System.out.println(Character.isLetter(23));
         System.out.println(Character.isAlphabetic(23));
-        String s1 = "hello";
+        System.out.println(Character.isLowerCase(23));
+        System.out.println(Character.isUpperCase(23));
+        System.out.println(Character.isTitleCase(23));
+        System.out.println(Character.isDigit(23));
 
+        String hey = "ABCabc";
+        byte[] heyBytes = hey.getBytes(); // [65,66,67,97,98,99]
+        System.out.println("heyBytes : " + heyBytes.toString());
     }
 
 }
