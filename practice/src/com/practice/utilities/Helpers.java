3a0208b10c875b66adfce187ecdc3f1dde203de1
Boula Akladyous <akladyous@gmail.com>
2023-03-09 09:15:17 -0500

    Untracked files on practice

--- practice/src/com/practice/utilities/Helpers.java
+++ practice/src/com/practice/utilities/Helpers.java
@@ -1,10 +1,15 @@
 package com.practice.utilities;
 
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Helpers {
     public static void main(String[] args) {
         currencyPrinter(234.11339);
+
+
     }
 
     public static void currencyPrinter(double number) {
