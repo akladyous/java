bfeb3e76f266cb155d3f7b574c42ed4096ad6c8d
Boula Akladyous <akladyous@gmail.com>
2023-03-16 15:58:09 -0400

    Untracked files on java

--- practice/src/com/practice/interfaces/demo/DemoClass.java
+++ practice/src/com/practice/interfaces/demo/DemoClass.java
@@ -4,10 +4,19 @@ public class DemoClass implements DemoInterface{
 
     @Override
     public void abstractMethod() {
-        System.out.println("Override abstract method");
+        System.out.println("1- Public Abstract Method");
     }
 
     public static void main(String[] args) {
+        System.out.println("all interface methods are by default : public");
+        System.out.println("All interface fields  are by default : " + DemoInterface.fieldString);
+        System.out.println("The interface body can contain:\n1-abstract methods, \n2-default methods, \n3-and static methods.");
+        System.out.println("-".repeat(50));
+
+        DemoClass demo = new DemoClass();
+        demo.abstractMethod();
+        demo.defaultMethod();
+        DemoInterface.StaticMethod();
 
     }
 }
--- practice/src/com/practice/interfaces/demo/DemoInterface.java
+++ practice/src/com/practice/interfaces/demo/DemoInterface.java
@@ -2,13 +2,16 @@ package com.practice.interfaces.demo;
 
 public interface DemoInterface {
 
-    public static final String fieldString = "public static final 'field'";
+    public static final String fieldString = "Public Static Final";
 
-    void abstractMethod();
+    public abstract void abstractMethod();
+    public default void defaultMethod(){
+        System.out.println("2- Default method");
+        System.out.println("   default methods are methods that can have a body");
+    };
 
-
-    public static void publicStatic() {
-        System.out.println("public static method");
+    public static void StaticMethod() {
+        System.out.println("3- static method");
     }
     private static void privateStaticMethod(){
         System.out.println("private static method");
