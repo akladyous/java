6b0ef729d6759ad68e361eb00ccdfef89b88ca67
Boula Akladyous <akladyous@gmail.com>
2023-03-17 23:27:40 -0400

    Untracked files on java

--- practice/src/com/practice/utilities/LookForPorts.java
+++ practice/src/com/practice/utilities/LookForPorts.java
@@ -1,8 +1,32 @@
 package com.practice.utilities;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class LookForPorts {
 
     public static void main(String[] args) {
 
+        Socket theSocket;
+        String host = "localhost";
+
+        if (args.length > 0) {
+            host = args[0];
+        }
+        for (int i = 0; i < 1024; i++) {
+            try {
+                System.out.println("Looking for "+ i);
+                theSocket = new Socket(host, i);
+                System.out.println("There is a server on port " + i + " of " + host);
+            }
+            catch (UnknownHostException e) {
+                System.err.println(e);
+                break;
+            }
+            catch (IOException e) {
+                // must not be a server on this port
+            }
+        }
     }
 }
