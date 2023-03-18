4c74f504c45c087792bef928b07db73efaeade54
Boula Akladyous <akladyous@gmail.com>
2023-03-18 09:54:36 -0400

    Untracked files on java

--- practice/src/com/practice/collection/deque/stack/StackDemo.java
+++ practice/src/com/practice/collection/deque/stack/StackDemo.java
@@ -1,15 +1,23 @@
-package com.practice.collection.deque.stack;
+package com.practice.collection.deque.deque;
 
 import java.util.*;
 
 public class StackDemo {
     public static void main(String[] args) {
-        Deque<Integer> stack = new ArrayDeque<>();
-        stack.add(1);
-        stack.push(2);
-        stack.pop();
-        Integer peeked = stack.peek();
-        System.out.println(peeked);
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.add(1); // add at the tail -> queue FIFO
+        deque.add(2); // add at the tail -> queue FIFO
+        System.out.println(deque);
+        deque.push(3); // add at the head -> stack LIFO
+        System.out.println(deque);
+        deque.pop(); // remove from the stack -> LIFO
+        System.out.println(deque);
+        deque.remove(); // remove element at the head -> FIFO
+        System.out.println(deque);
+
+
+
+
     }
 
 }
