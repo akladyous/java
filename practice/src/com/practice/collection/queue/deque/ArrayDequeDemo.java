651257331c8e6832ac1812bc1e49742bf3ce3e4b
Boula Akladyous <akladyous@gmail.com>
2023-03-19 14:35:19 -0400

    Untracked files on java

--- practice/src/com/practice/collection/queue/deque/ArrayDequeDemo.java
+++ practice/src/com/practice/collection/queue/deque/ArrayDequeDemo.java
@@ -8,10 +8,24 @@ public class ArrayDequeDemo {
 
     public static void main(String[] args) {
 
-        Deque<String> line = new ArrayDeque<>(List.of("a","b","c","d"));
-
+        Deque<String> line = new ArrayDeque<>(List.of("a","b","c","d","e"));
         System.out.println(line);
-        System.out.println(line.peek());
-        System.out.println(line.element());
+        line.add("1"); // insert element at the tail of queue. throw exception
+        line.offer("2");
+        System.out.println(line);
+
+        System.out.println(line.peek()); // Retrieves, but does not remove, the head of this queue,
+        System.out.println(line.element()); // Retrieves, but does not remove, the head of this queue. throw exception.
+
+        line.remove(); // Retrieves and removes the head of this queue. throw exception
+        line.poll(); // Retrieves and removes the head of this queue, or returns null if this queue is empty.
+        System.out.println(line);
+
+        line.addFirst("0");
+        System.out.println(line);
+        line.addLast("9");
+        System.out.println(line);
+
+
     }
 }
