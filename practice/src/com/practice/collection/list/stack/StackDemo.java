3b0adffdbb95d1b6610555ea7d3b4b6022f35997
Boula Akladyous <akladyous@gmail.com>
2023-03-19 14:51:09 -0400

    Untracked files on list

--- practice/src/com/practice/collection/list/stack/StackDemo.java
+++ practice/src/com/practice/collection/list/stack/StackDemo.java
@@ -1,8 +1,21 @@
 package com.practice.collection.list.stack;
 
+import java.util.List;
+import java.util.Stack;
+
 public class StackDemo {
 
     public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+
+        stack.push("a");
+        stack.push("b");
+        stack.push("c");
+
+        System.out.println("peek method: " + stack.peek());
+        System.out.println(stack);
+        stack.pop();
+        System.out.println(stack);
 
     }
 }
