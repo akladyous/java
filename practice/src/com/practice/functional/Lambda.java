626b068cddad27a3bf3007eaac83b52ac9f982a9
Boula Akladyous <akladyous@gmail.com>
2023-03-23 09:45:46 -0400

    Untracked files on java

--- practice/src/com/practice/functional/Lambda.java
+++ practice/src/com/practice/functional/Lambda.java
@@ -1,10 +1,22 @@
 package com.practice.functional;
 
+import java.util.List;
+
 public class Lambda {
 
+
+    public static void printUsers(List<User> users, Predicate<User> tester) {
+        for ( User u : users ) {
+            if ( tester.test(u)) {
+                System.out.println("Adult");
+            } else {
+                System.out.println("Minor");
+            }
+        }
+
+    }
 }
 
-
-interface CheckPerson {
-    boolean test();
+interface Predicate<T> {
+    boolean test(T t);
 }
