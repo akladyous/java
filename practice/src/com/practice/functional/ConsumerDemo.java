d942b98c184044a5f07551d5e83f95a63024e445
Boula Akladyous <akladyous@gmail.com>
2023-03-23 11:58:24 -0400

    Untracked files on java

--- practice/src/com/practice/functional/ConsumerDemo.java
+++ practice/src/com/practice/functional/ConsumerDemo.java
@@ -7,7 +7,6 @@ public class ConsumerDemo {
     public static void main(String[] args) {
 
         Consumer<String> printer = (string) -> System.out.println(string);
-
         printer.accept("hello");
     }
 
