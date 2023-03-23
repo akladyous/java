45d92d717f4a9065f5f38aeed7f704591e930bb6
Boula Akladyous <akladyous@gmail.com>
2023-03-23 12:02:46 -0400

    Untracked files on java

--- practice/src/com/practice/functional/ConsumerDemo.java
+++ practice/src/com/practice/functional/ConsumerDemo.java
@@ -1,13 +1,27 @@
 package com.practice.functional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerDemo {
+
+    public static List<Integer> numbers = new ArrayList<>();
+    static  {
+        numbers.addAll(List.of(1,2,3,4,5));
+    }
 
     public static void main(String[] args) {
 
         Consumer<String> printer = (string) -> System.out.println(string);
         printer.accept("hello");
     }
+
+    public static <T> void forEach(List<T> list,  Consumer<T> consumer) {
+        for (T element : list) {
+            consumer.accept(element);
+        }
+    }
+
 
 }
