7402ed8349b8eb7e497672bacac914fabb848b28
Boula Akladyous <akladyous@gmail.com>
2023-03-24 13:25:10 -0400

    Untracked files on java

--- practice/src/com/practice/reflection/FieldsAttributes.java
+++ practice/src/com/practice/reflection/FieldsAttributes.java
@@ -1,27 +1,24 @@
 package com.practice.reflection;
 
-import com.practice.klass.Reflect;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
 public class FieldsAttributes {
 
-
-    public static void getFiledsAttributes(Class c) {
+    public static void getFieldsAttribute(Class c) {
 
         System.out.println("-".repeat(80));
         System.out.printf("%-25s%-10s%-25s%n", "method", "type","modifier");
         System.out.println("-".repeat(80));
 
-        Field[] fields = c.getClass().getDeclaredFields();
+        Field[] fields = c.getDeclaredFields();
         for (Field field : fields) {
             String fieldName = field.getName();
             int modifier = field.getModifiers();
             String modifierName;
-            if ( ! Modifier.isPublic(modifier) &&
-                         ! Modifier.isPrivate(modifier) &&
-                         ! Modifier.isProtected(modifier)
+            if (! Modifier.isPublic(modifier) &&
+                        ! Modifier.isPrivate(modifier) &&
+                        ! Modifier.isProtected(modifier)
             ) {
                 modifierName = "Package-Private";
             } else {
@@ -38,6 +35,7 @@ public class FieldsAttributes {
                     fieldType,
                     modifierName
             );
+        }
     }
 
 }
---
+++ practice/src/com/practice/reflection/ReflectionDemo.java
@@ -0,0 +1,11 @@
+package com.practice.reflection;
+
+import com.practice.klass.Reflect;
+
+public class ReflectionDemo {
+
+
+    public static void main(String[] args) {
+        FieldsAttributes.getFieldsAttribute(Reflect.class);
+    }
+}
