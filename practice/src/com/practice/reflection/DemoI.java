3c168b1e83c638a9ad10da0cd9ffbc5abe940f6a
Boula Akladyous <akladyous@gmail.com>
2023-03-24 12:10:10 -0400

    Untracked files on java

--- practice/src/com/practice/reflection/DemoI.java
+++ practice/src/com/practice/reflection/DemoI.java
@@ -11,39 +11,61 @@ import java.lang.reflect.Modifier;
 public class DemoI {
     public static void main(String[] args) {
 
-        Field[] fields = Reflect.class.getDeclaredFields();
 
+        System.out.println("-".repeat(80));
+        System.out.printf("%-25s%-10s%-25s%n", "method", "type","modifier");
+        System.out.println("-".repeat(80));
+        Field[] fields = Reflect.class.getDeclaredFields();
         for (Field field : fields) {
             String fieldName = field.getName();
+            int modifier = field.getModifiers();
             String modifierName;
-            if ( ! Modifier.isPublic(field.getModifiers()) &&
-                 ! Modifier.isPrivate(field.getModifiers()) &&
-                 ! Modifier.isProtected(field.getModifiers())
+            if ( ! Modifier.isPublic(modifier) &&
+                 ! Modifier.isPrivate(modifier) &&
+                 ! Modifier.isProtected(modifier)
             ) {
                 modifierName = "Package-Private";
             } else {
-                modifierName = Modifier.toString(field.getModifiers());
+                modifierName = Modifier.toString(modifier);
             }
 
-//            String modifierName = Modifier.toString(field.getModifiers());
+//            String modifierName = Modifier.toString(modifier);
             String fieldType = field.getType().getSimpleName();
-            Modifier.isFinal(field.getModifiers());
+            Modifier.isFinal(modifier);
 
             System.out.printf(
-                    "Field: %-20s type: %-6s modifier: %-3s%n", fieldName, fieldType, modifierName
+                    "%-25s%-10s%-3s%n",
+                    fieldName,
+                    fieldType,
+                    modifierName
             );
         }
-        System.out.println("-".repeat(50));
 
+        System.out.println("-".repeat(80));
+        System.out.printf("%-25s%-10s%-25s%n", "method", "type","modifier");
+        System.out.println("-".repeat(80));
         Method[] methods = FunctionDemo.class.getMethods();
         for (Method method : methods) {
-            method.getModifiers();
-            Modifier.classModifiers();
-            System.out.println(
-                    "Method ( " +
-                            method.getName() +
-                            " ) Modifier: " +
-                            Modifier.toString(method.getModifiers())
+            String methodName = method.getName();
+            Class<?> returnType = method.getReturnType();
+            int modifiers = method.getModifiers();
+            String modifierName;
+
+            if ( !Modifier.isPublic(modifiers) &&
+                 !Modifier.isPrivate(modifiers) &&
+                 !Modifier.isProtected(modifiers)
+            ) {
+                modifierName = "Package-Private";
+            } else {
+                modifierName = Modifier.toString(modifiers);
+            }
+
+
+            System.out.printf(
+                    "%-25s%-10s%-3s%n",
+                    methodName,
+                    returnType.getSimpleName(),
+                    modifierName
             );
         }
     }
