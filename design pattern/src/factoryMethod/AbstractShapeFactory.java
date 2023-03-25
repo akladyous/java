443e06c3541f4ee55f36036d4c8ed67b803bc917
Boula Akladyous <akladyous@gmail.com>
2023-03-25 17:41:50 -0400

    Untracked files on java

--- design pattern/src/factoryMethod/AbstractShapeFactory.java
+++ design pattern/src/factoryMethod/AbstractShapeFactory.java
@@ -2,25 +2,31 @@ package factoryMethod;
 
 public abstract class AbstractShapeFactory {
     protected abstract Shape factoryMethod();
+
     public Shape getShape() {
+        System.out.println("get shape");
         return factoryMethod();
     }
 };
+
 class RectangleFactory extends AbstractShapeFactory {
     @Override
     protected Shape factoryMethod() {
         return new Rectangle();
     }
 }
+
 class SquareFactory extends AbstractShapeFactory {
     @Override
     protected Shape factoryMethod() {
         return new Square();
     }
 }
+
 class CircleFactory extends AbstractShapeFactory {
     @Override
     protected Shape factoryMethod() {
+        System.out.println("factory method");
         return new Circle();
     }
 }
