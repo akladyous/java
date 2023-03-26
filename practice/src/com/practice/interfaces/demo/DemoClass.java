c64d6656ead9f4c34eeb1e800f06ea66824fb0dc
Boula Akladyous <akladyous@gmail.com>
2023-03-26 16:03:38 -0400

    Untracked files on java

--- practice/src/com/practice/interfaces/demo/DemoClass.java
+++ practice/src/com/practice/interfaces/demo/DemoClass.java
@@ -7,20 +7,23 @@ public class DemoClass implements DemoInterface{
         System.out.println("1- Public Abstract Method");
     }
 
+
     public static void main(String[] args) {
         System.out.println("all interface methods are by default : public");
         System.out.println("All interface fields  are by default : " + DemoInterface.fieldString);
         System.out.println("The interface body can contain:\n1-abstract methods, \n2-default methods, \n3-and static methods.");
         System.out.println("-".repeat(50));
         System.out.println("invoking methods : ");
+
         DemoClass demo = new DemoClass();
         demo.abstractMethod();
         demo.defaultMethod();
-        DemoInterface.StaticMethod();
+        DemoInterface.staticMethod();
+
         System.out.println("-".repeat(50));
         System.out.println("Defining Private Methods in Interfaces");
         System.out.println("Private methods can be implemented static or non-static.");
-        System.out.println("This means that in an interface we are able to create private methods to encapsulate code from both default and static public method signatures");
+        System.out.println("This means that in an interface we are able to create private methods to encapsulate code from both 'default' and 'static' public method signatures");
 
     }
 }
--- practice/src/com/practice/interfaces/demo/DemoInterface.java
+++ practice/src/com/practice/interfaces/demo/DemoInterface.java
@@ -1,9 +1,7 @@
 package com.practice.interfaces.demo;
 
 public interface DemoInterface {
-
     public static final String fieldString = "Public Static Final";
-
     public abstract void abstractMethod();
     public default void defaultMethod(){
         System.out.println("2- Default method");
@@ -12,14 +10,14 @@ public interface DemoInterface {
         privateStaticMethod();
         privateMethod();
     };
-
-
-    public static void StaticMethod() {
+    public static void staticMethod() {
         System.out.println("3- static method");
+        privateStaticMethod();
     }
     private static void privateStaticMethod(){
         System.out.println("private static method");
     };
+    // private
     private void privateMethod() {
         System.out.println("private method");
     }
