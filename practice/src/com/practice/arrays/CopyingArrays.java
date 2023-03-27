e0eaa6654069a6abbc15b13c90f4a8b4bea95fe5
Boula Akladyous <akladyous@gmail.com>
2023-03-27 10:14:33 -0400

    Untracked files on java

--- practice/src/com/practice/arrays/CopyingArrays.java
+++ practice/src/com/practice/arrays/CopyingArrays.java
@@ -1,8 +1,15 @@
 package com.practice.arrays;
 
+import java.util.Arrays;
+
 public class CopyingArrays {
 
     public static void main(String[] args) {
+
+        int[] intArray1 = {1,2,3};
+        int[] intArray2 = Arrays.copyOf(intArray1, intArray1.length);
+
+
 
     }
 }
