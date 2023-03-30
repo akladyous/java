82c8ab247ccb57d288e0ca9acd793c428f307c9f
Boula Akladyous <akladyous@gmail.com>
2023-03-30 11:12:00 -0400

    Untracked files on java

--- practice/src/com/practice/generics/bounded_type_parameters/down_bounds/DownBound.java
+++ practice/src/com/practice/generics/bounded_type_parameters/down_bounds/DownBound.java
@@ -1,8 +1,20 @@
 package com.practice.generics.bounded_type_parameters.down_bounds;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class DownBound {
 
     public static void main(String[] args) {
 
+        List<String> words = new ArrayList<>(Arrays.asList("one", "two", "three", "four", "five"));
+
+        words.forEach((String str) -> {
+            System.out.println(str);
+        });
+
     }
+
+
 }
