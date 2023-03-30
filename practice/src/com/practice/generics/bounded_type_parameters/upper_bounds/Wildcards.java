1d0631ee337d3db6312fd035bb2cfec223131c0e
Boula Akladyous <akladyous@gmail.com>
2023-03-30 11:14:52 -0400

    Untracked files on practice

--- practice/src/com/practice/generics/bounded_type_parameters/upper_bounds/Wildcards.java
+++ practice/src/com/practice/generics/bounded_type_parameters/upper_bounds/Wildcards.java
@@ -1,4 +1,29 @@
 package com.practice.generics.bounded_type_parameters.upper_bounds;
 
+import java.util.List;
+
 public class Wildcards {
+
+    public static void main(String[] args) {
+        List<Integer> nums = List.of(1,2,3);
+        List<String> strings = List.of("a","b","c");
+        printList(nums);
+        printList(strings);
+    }
+
+    public static Integer sumListOfNums(List<? extends Number> nums) {
+        int sum = 0;
+        for (Number number : nums) {
+            sum += number.intValue();
+        }
+        return sum;
+    }
+
+
+    public static void printList(List<?> list) {
+        for (Object obj : list) {
+            System.out.println("object : " + obj);
+        }
+    };
+
 }
