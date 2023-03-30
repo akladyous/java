d6933cd8955f914853955f7b839c498f90caf3d9
Boula Akladyous <akladyous@gmail.com>
2023-03-30 10:01:19 -0400

    Untracked files on java

--- practice/src/com/practice/generics/bounded_type_parameters/upper_bounds/SumList.java
+++ practice/src/com/practice/generics/bounded_type_parameters/upper_bounds/SumList.java
@@ -1,7 +1,14 @@
 package com.practice.generics.bounded_type_parameters.upper_bounds;
 
+import java.util.List;
+
 public class SumList {
     public static void main(String[] args) {
 
+
+    }
+
+    public static double sumList(List<? extends Number> list) {
+        return list.stream().mapToDouble(Number::doubleValue).sum();
     }
 }
