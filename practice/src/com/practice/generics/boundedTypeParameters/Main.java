bd69c72ea5a8d6ab4ae851d20af74ea3f6ba1393
Boula Akladyous <akladyous@gmail.com>
2023-03-30 12:21:06 -0400

    Untracked files on practice

--- practice/src/com/practice/generics/boundedTypeParameters/Main.java
+++ practice/src/com/practice/generics/boundedTypeParameters/Main.java
@@ -1,6 +1,8 @@
 package com.practice.generics.boundedTypeParameters;
 
 
+import com.practice.generics.upper_bound.Box;
+
 // https://docs.oracle.com/javase/tutorial/java/generics/bounded.html
 public class Main {
 
@@ -8,6 +10,11 @@ public class Main {
         AddTwoNums<Integer> addTwoNums = new AddTwoNums<>(1,1);
         Number result = addTwoNums.sum();
         System.out.println(result);
+
+
+        Box<Integer> box = new Box<>();
+        box.setT(1);
+        box.inspect(2);
 
     }
 }
