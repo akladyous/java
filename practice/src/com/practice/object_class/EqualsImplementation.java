98d7f52de7d82e8f7847c93de0fe3f7fef0b8791
Boula Akladyous <akladyous@gmail.com>
2023-03-31 00:23:19 -0400

    Untracked files on java

--- practice/src/com/practice/object_class/EqualsImplementation.java
+++ practice/src/com/practice/object_class/EqualsImplementation.java
@@ -2,20 +2,20 @@ package com.practice.object_class;
 
 import java.util.Objects;
 
-public class ObjectMethodsDemo {
+public class EqualsImplementation {
     private int id;
     private String str;
     private int[] nums;
 
-    public ObjectMethodsDemo(int id, String str, int[] nums) {
+    public EqualsImplementation(int id, String str, int[] nums) {
         this.id = id;
         this.str = str;
         this.nums = nums;
     }
 
     public static void main(String[] args) {
-        ObjectMethodsDemo obj1 = new ObjectMethodsDemo(1, "a", new int[]{1,2,3});
-        ObjectMethodsDemo obj2 = new ObjectMethodsDemo(1, "a", new int[]{1,2,3});
+        EqualsImplementation obj1 = new EqualsImplementation(1, "a", new int[]{1,2,3});
+        EqualsImplementation obj2 = new EqualsImplementation(1, "a", new int[]{1,2,3});
 
         obj1.equals(obj2);
 
@@ -23,9 +23,14 @@ public class ObjectMethodsDemo {
 
     @Override
     public boolean equals(Object obj) {
+        // It is reflexive: for any non-null reference value x, x.equals(x) should return true.
         if (this == obj) return true;
+
+        // It is symmetric: for any non-null reference values x and
+        // y, x.equals(y) should return true if and only if y.equals(x) returns true.
+
         if (obj == null || this.getClass() != obj.getClass()) return false;
-        ObjectMethodsDemo that = (ObjectMethodsDemo) obj;
+        EqualsImplementation that = (EqualsImplementation) obj;
 
         if (!Objects.equals(this.id, that.id)) return false;
         if (! Objects.deepEquals(this.nums, that.nums)) return false;
---
+++ practice/src/com/practice/object_class/Person.java
@@ -0,0 +1,18 @@
+package com.practice.object_class;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    public String name;
+    public int age;
+    public int[] favoriteNumbers;
+
+    public Person() {}
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
