06ab2a71689992537a9fd0a7f71cf607106d45ea
Boula Akladyous <akladyous@gmail.com>
2023-03-31 18:28:00 -0400

    Untracked files on java

--- practice/src/com/practice/object_class/Person.java
+++ practice/src/com/practice/object_class/Person.java
@@ -66,6 +66,7 @@ public class Person {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
+
         result = prime * result + ( name == null ? 0 : name.hashCode());
         return result;
     }
