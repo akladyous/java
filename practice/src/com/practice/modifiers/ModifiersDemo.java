c68f663283e2cb056c7e08fbea32662b1ba4551d
Boula Akladyous <akladyous@gmail.com>
2023-04-01 15:06:37 -0400

    Untracked files on practice

--- practice/src/com/practice/modifiers/ModifiersDemo.java
+++ practice/src/com/practice/modifiers/ModifiersDemo.java
@@ -1,9 +1,15 @@
 package com.practice.modifiers;
 
+import com.practice.reflection.FieldsModifier;
+
 public class ModifiersDemo {
     final static Integer varInt = 1;
     static String varStr = new String("hello");
+    void prova(){};
+
+
     public static void main(String[] args) {
-        varStr = "sdfsdfsd";
+        FieldsModifier.getFieldsModifier(ModifiersDemo.class);
     }
+
 }
