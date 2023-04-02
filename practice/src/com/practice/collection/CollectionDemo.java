59e54db671cf99825ac3a5f6831dae2ffe67624c
Boula Akladyous <akladyous@gmail.com>
2023-04-02 15:06:06 -0400

    Untracked files on practice

--- practice/src/com/practice/collection/CollectionDemo.java
+++ practice/src/com/practice/collection/CollectionDemo.java
@@ -1,5 +1,23 @@
 package com.practice.collection;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CollectionDemo {
 
+    public static void main(String[] args) {
+
+        List<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.addAll(List.of(2,3,4,5));
+        System.out.println(nums);
+        nums.clear();
+        nums.addAll(List.of(1,2,3,4,5));
+        System.out.println("nums contains 1 : " + nums.contains(1));
+        System.out.println("nums contains 2,3,4 " + nums.containsAll(List.of(2,3,4)));
+        List<Integer> nums2 = new ArrayList<>(List.of(1,2,3,4,5));
+        System.out.println(nums.equals(nums2));
+
+    }
 }
