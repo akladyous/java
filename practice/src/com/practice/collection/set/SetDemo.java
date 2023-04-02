d465d936e7374548ddb108a86b4283dbb61bd542
Boula Akladyous <akladyous@gmail.com>
2023-04-01 21:00:34 -0400

    Untracked files on practice

--- practice/src/com/practice/collection/set/SetDemo.java
+++ practice/src/com/practice/collection/set/SetDemo.java
@@ -1,16 +1,22 @@
 package com.practice.collection.set;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class SetDemo {
     public static void main(String[] args) {
-        Set<Integer> set1 = new HashSet<>();
-        set1.add(1);
-        set1.add(null);
-        set1.add(null);
-        set1.add(null);
-        System.out.println(set1);
-
+        Set<Integer> set = new HashSet<>();
+        set.addAll(Arrays.asList(1,2,3,4));
+        set.clear();
+        set.add(5);
+        set.addAll(List.of(6,7,8,9,10));
+        set.add(null);
+        System.out.println(set);
+        System.out.println("contains method : " + set.contains(5));;
+        set.remove(5);
+        System.out.println(set);
     }
+
 }
