34c5c05e09ebbf0e2ed3bbfe58fb8a6534c60e0c
Boula Akladyous <akladyous@gmail.com>
2023-04-02 15:11:35 -0400

    Untracked files on practice

--- practice/src/com/practice/collection/CollectionDemo.java
+++ practice/src/com/practice/collection/CollectionDemo.java
@@ -17,7 +17,11 @@ public class CollectionDemo {
         System.out.println("nums contains 1 : " + nums.contains(1));
         System.out.println("nums contains 2,3,4 " + nums.containsAll(List.of(2,3,4)));
         List<Integer> nums2 = new ArrayList<>(List.of(1,2,3,4,5));
-        System.out.println(nums.equals(nums2));
+        System.out.println("list equals method nums1.equals(nums2) : " + nums.equals(nums2));
+        List<String> languages = new ArrayList<>();
+        languages.addAll(Arrays.asList("JavaScript", "Java", "C++", "Ruby", "Python", "Go", "Rust"));
+        languages.retainAll(Arrays.asList("Java"));
+        System.out.println(languages);
 
     }
 }
