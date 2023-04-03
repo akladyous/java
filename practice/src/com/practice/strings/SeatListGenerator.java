babfc7072e7f178ff38eaa6c26bbe1d648abf084
Boula Akladyous <akladyous@gmail.com>
2023-04-03 11:41:26 -0400

    Untracked files on practice

--- practice/src/com/practice/strings/SeatListGenerator.java
+++ practice/src/com/practice/strings/SeatListGenerator.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class SeatListGenerator {
 
+    public static void main(String[] args) {
+        System.out.println(generateSeatList(10, 4, 'A'));;
+    }
+
     public static List<String> generateSeatList(int seatPerRows, int totalRows, char rowPrefix) {
         List<String> seats = new ArrayList<>();
         int lastRow = (int) Character.valueOf(rowPrefix) + totalRows - 1; // 'A'-> 65 + 4-3 = 68 which is 'C'
