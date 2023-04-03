2f0658788337854ea28c540b087b1cc797a364ba
Boula Akladyous <akladyous@gmail.com>
2023-04-03 14:00:07 -0400

    Untracked files on java

--- practice/src/com/practice/collection/list/ArraList/ArrayListDemo.java
+++ practice/src/com/practice/collection/list/ArraList/ArrayListDemo.java
@@ -1,4 +1,29 @@
 package com.practice.collection.list.ArraList;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+
 public class ArrayListDemo {
+
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>();
+        nums.add(3);
+        if (nums.contains(3)) {
+            System.out.println("contains method Returns true if this list contains the specified element.");
+        }
+        nums.clear();
+        nums.addAll(List.of(1,2,3,4,5));
+
+
+
+    }
+
+    public static int randomNum(int maxNumber) {
+        Random random = new Random();
+        return random.nextInt(1, maxNumber);
+    };
+
+
 }
