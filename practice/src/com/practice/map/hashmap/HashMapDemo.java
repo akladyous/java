cac3d0884d2f3aeea1ca17383915dc7a00b297bc
Boula Akladyous <akladyous@gmail.com>
2023-04-05 17:00:56 -0400

    Untracked files on java

--- practice/src/com/practice/map/hashmap/HashMapDemo.java
+++ practice/src/com/practice/map/hashmap/HashMapDemo.java
@@ -42,9 +42,6 @@ public class HashMapDemo {
 
         map.keySet().forEach(key -> System.out.println("key : " + key));
         map.values().forEach(value -> System.out.println("value : " + value));
-        // ""
-        // ""
-        // ""
     }
 
     public static void demo1(){
