4e078683e6dae745b7862c1bc023037842f8fbac
Boula Akladyous <akladyous@gmail.com>
2023-04-06 17:50:57 -0400

    Untracked files on practice

--- practice/src/com/practice/concurency/Demo.java
+++ practice/src/com/practice/concurency/Demo.java
@@ -3,5 +3,7 @@ package com.practice.concurency;
 public class Demo {
     public static void main(String[] args) {
         Runtime rt = Runtime.getRuntime();
+        long usedKB = ( rt.totalMemory() - rt.freeMemory() ) / 1024;
+        System.out.format("Process ID : %d%n", ProcessHandle.current().pid());
     }
 }
