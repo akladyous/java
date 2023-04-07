22961c7007eea1a1242508d0dc8bf1e5a8588d17
Boula Akladyous <akladyous@gmail.com>
2023-04-07 14:46:22 -0400

    Untracked files on java

--- concurrency/src/App.java
+++ concurrency/src/App.java
@@ -16,6 +16,13 @@ class Runner2 implements Runnable {
 }
 public class App {
     public static void main(String[] args) {
+        Runner1 runner1 = new Runner1();
+        Runner2 runner2 = new Runner2();
+        Thread thread1 = new Thread(runner1);
+        Thread thread2 = new Thread(runner2);
+        thread1.start();
+        thread2.start();
+
 
     }
 }
