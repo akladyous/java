4c799e194d23adb20ae4cefe12f8261b41bf0a28
Boula Akladyous <akladyous@gmail.com>
2023-04-07 15:00:04 -0400

    Untracked files on java

--- concurrency/src/App.java
+++ concurrency/src/App.java
@@ -3,25 +3,32 @@ class Runner1 implements Runnable {
     public void run() {
         for ( int i = 0; i < 5; i++ ) {
             System.out.println("Runnable One : " + i);
+            for ( int j = 0; j < 10000; j++ ) {double x=i*i+Math.pow(i, 200);}
         }
     }
 }
-class Runner2 implements Runnable {
-    @Override
-    public void run() {
-        for ( int i = 0; i < 5; i++ ) {
-            System.out.println("Runnable Two : " + i);
-        }
-    }
-}
+
 public class App {
     public static void main(String[] args) {
         Runner1 runner1 = new Runner1();
-        Runner2 runner2 = new Runner2();
         Thread thread1 = new Thread(runner1);
-        Thread thread2 = new Thread(runner2);
+        Thread thread2 = new Thread(()->{
+            for ( int i = 0; i < 5; i++ ) {
+                System.out.println("Runnable Two : " + i);
+            }
+        });
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for ( int i = 0; i < 5; i++ ) {
+                    System.out.println("Runnable Three : " + i);
+                }
+            }
+        });
+
         thread1.start();
         thread2.start();
+        thread3.start();
 
 
     }
