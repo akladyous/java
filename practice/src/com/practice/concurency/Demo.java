1a2a9986c53a830d3abf7823e312d7eb235d0f88
Boula Akladyous <akladyous@gmail.com>
2023-04-06 20:36:38 -0400

    Untracked files on practice

--- practice/src/com/practice/concurency/Demo.java
+++ practice/src/com/practice/concurency/Demo.java
@@ -6,7 +6,7 @@ public class Demo {
         long usedKB = ( rt.totalMemory() - rt.freeMemory() ) / 1024;
         System.out.printf("Process ID %15d %n", ProcessHandle.current().pid());
         System.out.printf("Thread Count %15d %n", Thread.activeCount());
-        System.out.printf("total cores %15d %n", Runtime.getRuntime().availableProcessors(););
+        System.out.printf("total cores %15d %n", Runtime.getRuntime().availableProcessors());
         System.out.println("Memory Usage "+  usedKB);
     }
 }
