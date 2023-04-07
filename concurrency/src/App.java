3b1ae04d543869eb21adf878bb72066c06c63b6f
Boula Akladyous <akladyous@gmail.com>
2023-04-07 14:42:06 -0400

    Untracked files on java

--- concurrency/src/App.java
+++ concurrency/src/App.java
@@ -10,9 +10,12 @@ class Runner2 implements Runnable {
     @Override
     public void run() {
         for ( int i = 0; i < 5; i++ ) {
-
+            System.out.println("Runnable Two : " + i);
         }
     }
 }
 public class App {
+    public static void main(String[] args) {
+
+    }
 }
