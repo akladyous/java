511a647c01b618d507c526ccd5daaf2e8f8dfdcd
Boula Akladyous <akladyous@gmail.com>
2023-06-07 12:53:57 -0400

    Untracked files on java

--- practice/src/com/practice/exceptions/Checked.java
+++ practice/src/com/practice/exceptions/Checked.java
@@ -1,14 +1,20 @@
 package com.practice.exceptions;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class Checked {
 
     public static void main(String[] args) {
+        readFile("fileNotExists.txt");
 
     }
 
     private static void readFile (String fileName) {
-        FileReader reader = new FileReader(fileName);
+        try {
+            FileReader reader = new FileReader(fileName);
+        } catch(FileNotFoundException e) {
+            System.out.println("error opening the given file name");
+        }
     }
 }
