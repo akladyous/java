047b841a452ce39eede8a0fdbcee4db4428a3e6e
Boula Akladyous <akladyous@gmail.com>
2023-06-07 10:47:06 -0400

    Untracked files on java

---
+++ practice/src/com/practice/primitive/IntegerDemo.java
@@ -0,0 +1,12 @@
+package com.practice.primitive;
+
+public class IntegerDemo {
+    public static  void main(String[] args) {
+        byte b = 127;
+        short s = 32767;
+        int I = 10;
+        long l = 23000000;
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
