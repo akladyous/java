649e4f462c7bb8b9407a524894107feb9e05d1f3
Boula Akladyous <akladyous@gmail.com>
2023-06-17 18:46:13 -0400

    Untracked files on java

--- practice/src/com/practice/primitive/StringDemo.java
+++ practice/src/com/practice/primitive/StringDemo.java
@@ -3,41 +3,35 @@ package com.practice.primitive;
 import java.util.Arrays;
 
 public class StringDemo {
-    static int valueX;
+  String id;
+  String name;
 
-    static {
-        System.out.println("static block, get called once, when the class itself is initialized");
-    }
+  String newLine = System.getProperty("line.separator");
+  String prova = new String("ciao");
 
-    String id;
-    String name;
-    String newLine = System.getProperty("line.separator");
-    String prova = new String("ciao");
+  static int valueX;
 
-    {
-        System.out.println("non-static block, get called every time an instance of the class is constructed");
-    }
+  static { System.out.println("static block, get called once, when the class itself is initialized");     }
+         { System.out.println("non-static block, get called every time an instance of the class is constructed"); }
 
 
-    // overload methods
-    public static void main(String[] args) {
-        String s1 = "ciao a tutti belli e brutti";
-        String[] s2 = s1.split(" ");
-        System.out.println(Arrays.toString(s2));
+  // overload methods
+  public static void main(String[] args) {
+    String s1 = "ciao a tutti belli e brutti";
+    String[] s2 = s1.split(" ");
+    System.out.println(Arrays.toString(s2));
 
-        System.out.println("args = " + Arrays.toString(args));
+    System.out.println("args = " + Arrays.toString(args));
 
-    }
+  };
 
-    ;
+  @Override
+  public int hashCode() {
+    return super.hashCode();
+  }
 
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
+  @Override
+  public boolean equals(Object obj) {
+    return super.equals(obj);
+  }
 }
