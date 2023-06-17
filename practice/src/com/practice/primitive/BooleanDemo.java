8953b12cb9ee62d967146952953854997eeeb7f2
Boula Akladyous <akladyous@gmail.com>
2023-06-17 18:26:31 -0400

    Untracked files on java

--- practice/src/com/practice/primitive/BooleanDemo.java
+++ practice/src/com/practice/primitive/BooleanDemo.java
@@ -2,16 +2,13 @@ package com.practice.primitive;
 
 public class BooleanDemo {
   public static void main(String[] args) {
-    String boolStr = "true";
-    String boolTrue = "true";
-    String boolFalse = "true";
 
-    System.out.println("Boolean.valueOf() is not null : " + Boolean.valueOf(boolStr));
+    System.out.println("Boolean.valueOf() is not null : " + Boolean.valueOf("true"));
     System.out.println("Boolean.compare               : " + Boolean.compare(true, false));
-    System.out.println("compareTo                     : " + Boolean.valueOf(boolTrue).compareTo(Boolean.valueOf(boolFalse)));
+    System.out.println("compareTo                     : " + Boolean.valueOf("true").compareTo(Boolean.valueOf("false")));
     System.out.println("Boolean.logicalAnd            : " + Boolean.logicalAnd(true, true));
-    System.out.println("Boolean.getBoolean            : " + Boolean.getBoolean(boolStr));
-    System.out.println("Boolean.parse(string value)   : " + Boolean.parseBoolean(boolStr));
+    System.out.println("Boolean.getBoolean            : " + Boolean.getBoolean("true"));
+    System.out.println("Boolean.parse(string value)   : " + Boolean.parseBoolean("true"));
   }
 }
 
